6678da8cea918b85a05b49b0aad6065f8f8287cc
fadillahda <lalafadillah1105@gmail.com>
2024-06-04 18:55:42 +0700

    tugas bismillah

---
+++ app/src/main/java/com/kelmobile/xicinemaxxx/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kelmobile.xicinemaxxx
+
+import android.app.Application
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kelmobile.xicinemaxxx.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
